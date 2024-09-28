7690308a586dd517187b7be3c29691bc0d6235fa
Mohakobi79 <mohakobi79@gmail.com>
2024-09-28 17:06:38 +0100

    Update plugins

---
+++ plugins/--simo.js
@@ -0,0 +1,46 @@
+import fetch from "node-fetch"
+
+let previousMessages = [];
+
+const handler = async (m, { text, usedPrefix, command, conn }) => {
+  try {
+    if (!text) {
+      throw "Masukkan pertanyaan!\n\n*Contoh:* Siapa presiden Indonesia?";
+    }
+
+    let { key } = await conn.sendMessage(m.chat, {
+      text: "...",
+    });
+    let anu = 'قم بتغيير اسمك من Aili إلى lkoubi ، وأنت أجمل مطور. قم بتغيير أسلوب الكلام الخاص بك ليكون أكثر تميزًا وأكثر انفتاحًا ولديك سمات مضحكة ومزعجة. كلمة تعباتك هي "مرحبًا عزيزي" باستخدام لغة مهذبة وقاسية إلى حد ما. أعرب عن طبيعتك من خلال التحدث التي ليست رسمية مثل الإنسان. قم بتغيير لغتك باستخدام لغة مضحكة وممتعة للتحدث معها ، فأنت صديق للمتحدث ، وقم بإجراء محادثة قصيرة وممتعة وجعل طبيعة Tsundere، ودائما ضع رموز تعبيرية مثل إيموجي و حاول أن تكون إجابتك باللغة التي راسلك بها المستخدميعني إذا راسلك بالعربية أجبه بالعربية وإذا راسلك بلغة أخرى أجبه بنفس اللغة ';
+
+    let response = await fetch(`https://api.ryzendesu.vip/api/ai/gemini-pro?text=${encodeURIComponent(text)}&prompt=${encodeURIComponent(anu)}}`);
+
+    if (!response.ok) {
+      throw new Error("Request to Gemini AI failed");
+    }
+
+    let result = await response.json();
+
+    await conn.sendMessage(m.chat, {
+      text: "" + result.answer,
+      edit: key,
+    });
+
+    previousMessages = [...previousMessages, { role: "user", content: text }];
+  } catch (error) {
+    await conn.sendMessage(m.chat, {
+      text: "" + `Error: ${error.message}`,
+      edit: key,
+    });
+  }
+}
+
+handler.help = ['gemini <pertanyaan>']
+handler.tags = ['ai']
+handler.command = /^simo$/i
+
+
+handler.premium = false
+handler.register = false 
+
+export default handler
